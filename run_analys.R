f587666bdb622d79932f23366bc6364a8b821dbe
paolopezzolla <76875987+paolopezzolla@users.noreply.github.com>
2021-02-12 12:20:52 +0100

    Update run_analys.R

--- run_analys.R
+++ run_analys.R
@@ -3,46 +3,46 @@ library(dplyr)
 
 # set dataset directory)
 
-# read train data 
+# reading train data from directory
 x_train   <- read.table("./train/X_train.txt")
 y_train   <- read.table("./train/Y_train.txt") 
 sub_train <- read.table("./train/subject_train.txt")
 
-# read test data 
+# reading test data from directory
 x_test   <- read.table("./test/X_test.txt")
 y_test   <- read.table("./test/Y_test.txt") 
 sub_test <- read.table("./test/subject_test.txt")
 
-#read variable names
+#reading data description from directory
 variable_names <- read.table("./features.txt")
 
-# read activity labels 
+# reading activity labels 
 activity_labels <- read.table("./activity_labels.txt") 
 
-# merge of training and test sets
+# merging of training and test sets
 x_total   <- rbind(x_train, x_test)
 y_total   <- rbind(y_train, y_test) 
 sub_total <- rbind(sub_train, sub_test) 
 
-# analysis for mean and standard deviation 
+
 sel_features <- variable_names[grep(".*mean\\(\\)|std\\(\\)", features[,2], ignore.case = FALSE),]
 x_total      <- x_total[,sel_features[,1]]
 
-# name columns
+# col names
 colnames(x_total)   <- sel_features[,2]
 colnames(y_total)   <- "activity"
 colnames(sub_total) <- "subject"
 
-# merge final dataset
+# merging last dataset
 total <- cbind(sub_total, y_total, x_total)
 
-# turn activities & subjects into factors 
+# turning activities & subjects into factors 
 total$activity <- factor(total$activity, levels = activity_labels[,1], labels = activity_labels[,2]) 
 total$subject  <- as.factor(total$subject) 
 
-# create a summary independent tidy dataset from final dataset 
+# tidying dataset
 # with the average of each variable for each activity and each subject. 
 total_mean <- total %>% group_by(activity, subject) %>% summarize_each(funs(mean)) 
 
-# export summary dataset
+# texting file of final dataset
 write.table(total_mean, file = "./tidydata.txt", row.names = FALSE, col.names = TRUE) 
